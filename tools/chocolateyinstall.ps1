7d0405c5f1ad29535b2ea290901acdca7e5d5c9f
Jared Beach <j.beach@hylasoftusa.com>
2017-03-03 10:00:21 -0600

    init

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿
+$path = Join-Path "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 'MatrikonOPCSimulation.EXE'
+$name= 'matrikon-sim'
+$silent = '/s'
+
+Install-ChocolateyPackage $name 'exe' $silent $path
+ 
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,9 @@
+﻿
+$packageName = 'matrikon-sim'
+$softwareName = 'MatrikonOPC Server for Simulation*'
+$installerType = 'EXE' 
+$silentArgs = ' REMOVE=TRUE MODIFY=FALSE /s'
+$registryKey = "hklm:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\MatrikonOPC Server for Simulation"
+$uninstallString = (Get-ItemProperty $registryKey -name ModifyPath).ModifyPath
+
+Uninstall-ChocolateyPackage -PackageName $packageName -FileType 'EXE' -SilentArgs $silentArgs -File $uninstallString
